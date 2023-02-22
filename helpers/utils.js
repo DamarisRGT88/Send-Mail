6ff1dfb2407ae2671ac4339815afeeadebc20f4e
DamarisRGT88 <damarisrgt@gmail.com>
2023-02-22 12:03:24 +0100

    borrado

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,39 +1,6 @@
 const nodeMailer = require('nodemailer');
 
 
-// const sendEmail = (to, subject, message,  callback) => {
-//     const transporter = nodemailer.createTransport({
-//         host: "smtp.gmail.com",
-//         port: 465,
-//         secure: true,
-//         auth: {
-//             user: process.env.MAIL_USER,
-//             pass: process.env.MAIL_PASS,
-//         },
-//     });
-//     const mailDate = {
-//         from: process.env.MAIL_USER,
-//         subject,
-//         to,
-//         text: message,
-//         // html: notification
-//     };
-//     // const mailDate2 = {
-//     //     from: emailContact,
-//     //     to,
-//     //     subject,
-//     //     text: message,
-//     //     html: notification
-//     // }
-//     transporter.sendEmail(mailDate, callback);
-//     // transporter.sendEmail(mailDate2, callback)
-
-// }
-
-// module.exports = {
-//     sendEmail
-// }
-
 const sendMail = (to, subject, message, emailContact, callback) => {
     const transporter = nodeMailer.createTransport({
         service: "Gmail",
